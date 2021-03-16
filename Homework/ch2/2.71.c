ed5e2db2f1484dc5e39f8fd30f66f56d1b6a6b4a
石翔宇 <xyu.shi@outlook.com>
2021-03-16 14:49:09 +0800

    update

---
+++ Homework/ch2/2.71.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+typedef unsigned packed_t;
+
+int xbyte(packed_t word, int bytenum)
+{
+    
+}
+
+int main()
+{
+
+    return 0;
+}
---
+++ Lab1/datatype.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+char ch = 'a';
+int i = 1190200523;
+unsigned int ui = 1190200523;
+short sh = 1190200523;
+unsigned short ush = 1190200523;
+long l = 1190200523;
+unsigned long ul = 1190200523;
+float f = 1190200523;
+double d = 1190200523;
+long double ld = 1190200523;
+
+
+int main()
+{
+    
+    return 0;
+}
