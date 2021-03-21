a64fa06ebba2b71c6ff2197d4479b46d312c4859
Dedsecr <xyu.shi@outlook.com>
2021-03-21 10:47:14 +0800

    update

--- Lab/Lab1/Datatype.c
+++ Lab/Lab1/Datatype.c
@@ -27,12 +27,9 @@ enum color_set
 typedef unsigned char *byte_pointer;
 void show_bytes(byte_pointer start,size_t len)
 {
-    size_t i;
     printf("\tHex:");
-    for (i = 0; i < len; i++)
-    {
+    for (size_t i = 0; i < len; i++)
         printf(" %.2x", start[i]);
-    }
     printf("\n");
 }
 
