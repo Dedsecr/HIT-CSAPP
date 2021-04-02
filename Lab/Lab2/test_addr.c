26ce93039fe00306206fa452378160b2c0cca72b
石翔宇 <xyu.shi@outlook.com>
2021-04-02 16:27:51 +0800

    update

---
+++ Lab/Lab2/test_addr.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int x = -1190200523;
+
+int main()
+{
+    double y = 131182200003105535;
+    static char z[20] = "1190200523-石翔宇";
+    printf("x = %d\ny = %lf\nz = %s\n", x, y, z);
+    return 0;
+}
