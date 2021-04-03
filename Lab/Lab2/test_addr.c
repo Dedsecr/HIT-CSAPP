05d5f7274fb489ffea91aa2be43e5e6efd3db82d
石翔宇 <xyu.shi@outlook.com>
2021-04-03 10:58:34 +0800

    update

--- Lab/Lab2/test_addr.c
+++ Lab/Lab2/test_addr.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int x = -1190200523;
+const int x = -1190200523;
 
 int main()
 {
-    double y = 131182200003105535;
-    static char z[20] = "1190200523-石翔宇";
+    float y = 131182200003105535;
+    static char z[100] = "1190200523-石翔宇";
     printf("x = %d\ny = %lf\nz = %s\n", x, y, z);
     return 0;
 }
