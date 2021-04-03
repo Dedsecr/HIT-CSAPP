dc05a754a785524996a9a5dba12469bb2dc3641b
石翔宇 <xyu.shi@outlook.com>
2021-04-03 14:40:20 +0800

    update

---
+++ Lab/Lab2/test_argc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+const int x = -1190200523;
+
+int main(int argc, char *argv[])
+{
+    printf("%d\n", argc);
+    printf("%s\n", argv[1]);
+    printf("%s\n", argv[2]);
+    printf("%s\n", argv[3]);
+    return 0;
+}
