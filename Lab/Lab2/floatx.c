9e934a644ca4f2411a9d76c10c77a313fdf95518
石翔宇 <xyu.shi@outlook.com>
2021-04-09 00:08:55 +0800

    update

--- Lab/Lab2/floatx.c
+++ Lab/Lab2/floatx.c
@@ -25,12 +25,12 @@ int main()
     *pointer_minimum_positive_normalized_float = 0x00800000;
     *pointer_positive_infinity = 0x7f800000;
     *pointer_not_a_number = 0x7f800001;
-    printf("%f\t0x%x\n", positive_zero, *pointer_positive_zero);
-    printf("%f\t0x%x\n", negative_zero, *pointer_negative_zero);
-    printf("%f\t0x%x\n", minimum_positive_float, *pointer_minimum_positive_float);
-    printf("%f\t0x%x\n", maximum_positive_float, *pointer_maximum_positive_float);
-    printf("%f\t0x%x\n", minimum_positive_normalized_float, *pointer_minimum_positive_normalized_float);
-    printf("%f\t0x%x\n", positive_infinity, *pointer_positive_infinity);
-    printf("%f\t0x%x\n", not_a_number, *pointer_not_a_number);
+    printf("%f\t0x%.8x\n", positive_zero, *pointer_positive_zero);
+    printf("%f\t0x%.8x\n", negative_zero, *pointer_negative_zero);
+    printf("%f\t0x%.8x\n", minimum_positive_float, *pointer_minimum_positive_float);
+    printf("%f\t0x%.8x\n", maximum_positive_float, *pointer_maximum_positive_float);
+    printf("%f\t0x%.8x\n", minimum_positive_normalized_float, *pointer_minimum_positive_normalized_float);
+    printf("%f\t0x%.8x\n", positive_infinity, *pointer_positive_infinity);
+    printf("%f\t0x%.8x\n", not_a_number, *pointer_not_a_number);
     return 0;
 }
