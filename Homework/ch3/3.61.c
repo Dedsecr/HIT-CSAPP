f53810044e0724115ac1647ecabe6557002dc795
Dedsecr <xyu.shi@outlook.com>
2021-04-11 20:37:45 +0800

    update

--- Homework/ch3/3.61.c
+++ Homework/ch3/3.61.c
@@ -1,2 +1,6 @@
 #include <stdio.h>
 
+long cread_alt(long *xp)
+{
+    return (!xp ? 0 : *xp);
+}
--- Homework/ch3/3.69.c
+++ Homework/ch3/3.69.c
@@ -1,1 +1,11 @@
 #include <stdio.h>
+
+/*
+A. 7
+B.
+*/
+struct a_struct
+{
+    long idx;
+    long x[4];
+}
